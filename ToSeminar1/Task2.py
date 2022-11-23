3f552bd72c3f3551d94ba45d36cb8d057a4d1f44
Valeriya <wal.now@yandex.ru>
2022-11-23 11:14:13 +0300

    add Task2

--- ToSeminar1/Task2.py
+++ ToSeminar1/Task2.py
@@ -1,11 +1,14 @@
 # Напишите программу для. проверки истинности 
 # утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат
 
-print('x  y  z  ¬(X ⋁ Y ⋁ Z)   ¬X ⋀ ¬Y ⋀ ¬Z')
+print('x y z    ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z')
 for x in range(0,2):
     for y in range(0,2):
         for z in range(0,2):
-           print(f'{x}  {y}  {z}        {~ (x | y | z)}           {~ x & ~ y & ~ z }')
+           print(f'{x} {y} {z}       ', end=' ')
+           print ('1') if (~ (x | y | z) == ~ x & ~ y & ~ z ) else print ('0')
+
+
            
     
                 
