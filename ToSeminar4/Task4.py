26d1ae7fb108db5a985ee3b6ea47dba01343e5a4
Valeriya <wal.now@yandex.ru>
2022-12-06 23:25:33 +0300

    4

--- ToSeminar4/Task4.py
+++ ToSeminar4/Task4.py
@@ -4,24 +4,37 @@
 
 print('Введите степень многочлена k =')
 k = int(input())
+print('Введите степень многочлена p =')
+p = int(input())
 import random
-list_coefficient = [random.randint(0, 100) for x in range(k + 1)]
+list_coefficient = [random.randint(0, 101) for x in range(k + 1)]
 print(list_coefficient)
-
+list_coefficient2 = [random.randint(-99, 101) for x in range(p+ 1)]
+print(list_coefficient2)
 
 def full_equation(lst, n):
-    for i  in range(len(lst)-1):
-        print(f'{lst[i]}x^{n-i}', end=' + ')
-    print(f'{lst[n]}')
-    return ' '
+    array = []
+    for i  in range(len(lst)):
+       array.append(f'{lst[i]}*x^{n-i}')
+       
+    return array
         
-    
-print(full_equation(list_coefficient, k))
+beautiful_lst1 = ' + '.join(full_equation(list_coefficient, k))    
+print(beautiful_lst1)
+
+beautiful_lst2 = ' + '.join(full_equation(list_coefficient2, p))    
+print(beautiful_lst2)
 
 path = 'File1.txt'
 file = open(path, 'w')
-file.full_equation([1, 2, 5, 8], 3)
+file.write(beautiful_lst1)
 file.close()
 
-import os
-print (os.listdir(os.getcwd()))
+path = 'File2.txt'
+file = open(path, 'w')
+file.write(beautiful_lst2)
+file.close()
+
+
+# import os
+# print (os.listdir(os.getcwd()))
